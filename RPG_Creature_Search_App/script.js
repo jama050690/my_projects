54570092240991548780b69d695cfdfcf179337d
Jamshiddin <jbm050690@gmail.com>
2025-10-25 20:29:12 +0500

    add

--- RPG_Creature_Search_App/script.js
+++ RPG_Creature_Search_App/script.js
@@ -1,68 +1,68 @@
-const inputEl = document.getElementById("search-input");
-const searchBtnEl = document.getElementById("search-button");
-const nameEl = document.getElementById("creature-name");
-const idEl = document.getElementById("creature-id");
-const weightEl = document.getElementById("weight");
-const heightEl = document.getElementById("height");
-const hpEl = document.getElementById("hp");
-const attackEl = document.getElementById("attack");
-const defenseEl = document.getElementById("defense");
-const spAttackEl = document.getElementById("special-attack");
-const spDefenseEl = document.getElementById("special-defense");
-const speedEl = document.getElementById("speed");
-const typesEl = document.getElementById("types");
+const creatureID = document.getElementById("creature-id");
+const creatureName = document.getElementById("creature-name");
+const specialName = document.getElementById("special-name");
+const specialDescription = document.getElementById("special-description");
+const types = document.getElementById("types");
+const height = document.getElementById("height");
+const weight = document.getElementById("weight");
+const hp = document.getElementById("hp");
+const attack = document.getElementById("attack");
+const defense = document.getElementById("defense");
+const specialAttack = document.getElementById("special-attack");
+const specialDefense = document.getElementById("special-defense");
+const speed = document.getElementById("speed");
+const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("search-input");
 
-searchBtnEl.addEventListener("click", function () {
-  const qiymat = inputEl.value.trim().toLowerCase();
+const getCreature = async () => {
+  try {
+    const creatureNameOrId = searchInput.value.toLowerCase();
+    const response = await fetch(
+      `https://rpg-creature-api.freecodecamp.rocks/api/creature/${creatureNameOrId}`
+    );
+    const data = await response.json();
 
-  typesEl.textContent = "";
+    creatureName.textContent = `${data.name.toUpperCase()}`;
+    creatureID.textContent = `#${data.id}`;
+    weight.textContent = `Weight: ${data.weight}`;
+    height.textContent = `Height: ${data.height}`;
+    specialName.textContent = data.special.name;
+    specialDescription.textContent = data.special.description;
 
-  if (qiymat === "pyrolynx") {
-    nameEl.textContent = "PYROLYNX";
-    idEl.textContent = "#1";
-    weightEl.textContent = "Weight: 42";
-    heightEl.textContent = "Height: 32";
-    hpEl.textContent = "65";
-    attackEl.textContent = "80";
-    defenseEl.textContent = "50";
-    spAttackEl.textContent = "90";
-    spDefenseEl.textContent = "55";
-    speedEl.textContent = "100";
+    hp.textContent = data.stats[0].base_stat;
+    attack.textContent = data.stats[1].base_stat;
+    defense.textContent = data.stats[2].base_stat;
+    specialAttack.textContent = data.stats[3].base_stat;
+    specialDefense.textContent = data.stats[4].base_stat;
+    speed.textContent = data.stats[5].base_stat;
 
-    const typeItem = document.createElement("p");
-    typeItem.textContent = "FIRE";
-    typeItem.style.color = "orange";
-    typeItem.style.fontWeight = "bold";
-    typesEl.appendChild(typeItem);
-  } else if (qiymat === "aquoroc") {
-    nameEl.textContent = "AQUOROC";
-    idEl.textContent = "#2";
-    weightEl.textContent = "Weight: 220";
-    heightEl.textContent = "Height: 53";
-    hpEl.textContent = "85";
-    attackEl.textContent = "90";
-    defenseEl.textContent = "120";
-    spAttackEl.textContent = "70";
-    spDefenseEl.textContent = "95";
-    speedEl.textContent = "60";
-
-    const typeItem = document.createElement("p");
-    typeItem.textContent = "WATER/ROCK";
-    typeItem.style.color = "blue";
-    typeItem.style.fontWeight = "bold";
-    typesEl.appendChild(typeItem);
-  } else {
-    alert("Creature not found!");
-
-    nameEl.textContent = "";
-    idEl.textContent = "";
-    weightEl.textContent = "";
-    heightEl.textContent = "";
-    hpEl.textContent = "";
-    attackEl.textContent = "";
-    defenseEl.textContent = "";
-    spAttackEl.textContent = "";
-    spDefenseEl.textContent = "";
-    speedEl.textContent = "";
+    types.innerHTML = data.types
+      .map((obj) => `<span class="type ${obj.name}">${obj.name}</span>`)
+      .join("");
+  } catch (err) {
+    resetDisplay();
+    alert("Creature not found");
+    console.log(`Creature not found: ${err}`);
   }
+};
+
+const resetDisplay = () => {
+  creatureName.textContent = "";
+  creatureID.textContent = "";
+  height.textContent = "";
+  weight.textContent = "";
+  types.innerHTML = "";
+  specialName.innerHTML = "";
+  specialDescription.innerHTML = "";
+  hp.textContent = "";
+  attack.textContent = "";
+  defense.textContent = "";
+  specialAttack.textContent = "";
+  specialDefense.textContent = "";
+  speed.textContent = "";
+};
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getCreature();
 });
